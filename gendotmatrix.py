2722d44cc263c40688cd88c2ae884d882ef20eb5
mingang.he <dustgle@gmail.com>
2013-09-03 23:33:29 +0800

    modify get_pix()

--- gendotmatrix.py
+++ gendotmatrix.py
@@ -4,14 +4,15 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def get_pix(image):
+    pixel = image.load()
     width, height = image.size
     print width
     print height
     for h in range(height):
         for w in range(width):
-            # pix = image[w, h]
-            pix = image.getpixel((w, h))
-            print pix
+            print pixel[w, h]
+            # pix = image.getpixel((w, h))
+            # print pix
 
 def main():
     image = Image.new("RGB", (32, 16), (255, 255, 255))
@@ -19,8 +20,8 @@ def main():
     # usr_font = ImageFont.truetype("/usr/share/fonts/truetype/takao/TakaoPGothic.ttf", 25)
     d_usr = ImageDraw.Draw(image)
     d_usr.text((0, 0), u"汉字", (0,0,0), font=usr_font)
-    image.save("test.jpg")
     get_pix(image)
+    image.save("test.jpg")
 
 if __name__ == '__main__':
     main()
