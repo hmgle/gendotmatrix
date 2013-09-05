450b920b23438e22744cf9676442c5522ec1cc2d
mingang.he <dustgle@gmail.com>
2013-09-05 23:15:37 +0800

    add bitarray

--- gendotmatrix.py
+++ gendotmatrix.py
@@ -2,27 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image, ImageDraw, ImageFont
+from bitarray import bitarray
 
 def get_pix(image):
     pixel = image.load()
     width, height = image.size
     print width
     print height
+    bitmap = bitarray()
     for h in range(height):
         for w in range(width):
-            print pixel[w, h]
+            if int(sum(pixel[w, h])) > (255 * 3 / 2):
+                bitmap.append(False)
+            else:
+                bitmap.append(True)
             # pix = image.getpixel((w, h))
             # print pix
+    print bitmap
 
 def main():
-    image = Image.new("RGB", (32, 16), (255, 255, 255))
+    image = Image.new("RGB", (16, 16), (255, 255, 255))
     usr_font = ImageFont.truetype("/usr/share/fonts/truetype/wqy/wqy-microhei.ttc", 16)
     # usr_font = ImageFont.truetype("/usr/share/fonts/truetype/takao/TakaoPGothic.ttf", 25)
     d_usr = ImageDraw.Draw(image)
-    d_usr.text((0, 0), u"汉字", (0,0,0), font=usr_font)
+    d_usr.text((0, 0), u"字", (0,0,0), font=usr_font)
     get_pix(image)
     image.save("test.jpg")
 
 if __name__ == '__main__':
     main()
-
