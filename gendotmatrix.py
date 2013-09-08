fd0e3f49b35a6d186769aca3ce42b865898ab5f9
mingang.he <dustgle@gmail.com>
2013-09-08 11:27:36 +0800

    add get_gb2312_pix()

--- gendotmatrix.py
+++ gendotmatrix.py
@@ -20,6 +20,13 @@ def get_pix(image):
             # print pix
     print bitmap
 
+def get_gb2312_pix(gb2312_code, w, h, usr_font):
+    image = Image.new("RGB", (w, h), (255, 255, 255))
+    d_usr = ImageDraw.Draw(image)
+    unicode_code = gb2312_code.decode('gb2312')
+    d_usr.text((0, 0), unicode_code, (0,0,0), font=usr_font)
+    get_pix(image)
+
 def main():
     image = Image.new("RGB", (16, 16), (255, 255, 255))
     usr_font = ImageFont.truetype("/usr/share/fonts/truetype/wqy/wqy-microhei.ttc", 16)
@@ -27,6 +34,7 @@ def main():
     d_usr = ImageDraw.Draw(image)
     d_usr.text((0, 0), u"字", (0,0,0), font=usr_font)
     get_pix(image)
+    get_gb2312_pix('\xd7\xd6', 16, 16, usr_font)
     image.save("test.jpg")
 
 if __name__ == '__main__':
