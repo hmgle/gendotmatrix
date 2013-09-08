fdd759fb430c76335dc138f1ae134bfe0ba24832
mingang.he <dustgle@gmail.com>
2013-09-08 19:24:33 +0800

    output font_dot_matrix

--- gendotmatrix.py
+++ gendotmatrix.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image, ImageDraw, ImageFont
+import bitarray
 from bitarray import bitarray
 
 def get_pix(image):
     pixel = image.load()
     width, height = image.size
-    print width
-    print height
     bitmap = bitarray()
     for h in range(height):
         for w in range(width):
@@ -18,24 +17,34 @@ def get_pix(image):
                 bitmap.append(True)
             # pix = image.getpixel((w, h))
             # print pix
-    print bitmap
+    return bitmap
+    # with open('bin.tmp', 'wb') as myfile:
+    #     bitmap.tofile(myfile)
 
 def get_gb2312_pix(gb2312_code, w, h, usr_font):
     image = Image.new("RGB", (w, h), (255, 255, 255))
     d_usr = ImageDraw.Draw(image)
-    unicode_code = gb2312_code.decode('gb2312')
-    d_usr.text((0, 0), unicode_code, (0,0,0), font=usr_font)
-    get_pix(image)
+    try:
+        unicode_code = gb2312_code.decode('gb2312')
+        d_usr.text((0, 0), unicode_code, (0,0,0), font=usr_font)
+        return get_pix(image)
+    except:
+        d_usr.text((0, 0), u" ", (0,0,0), font=usr_font)
+        return get_pix(image)
 
 def main():
-    image = Image.new("RGB", (16, 16), (255, 255, 255))
+    # image = Image.new("RGB", (16, 16), (255, 255, 255))
     usr_font = ImageFont.truetype("/usr/share/fonts/truetype/wqy/wqy-microhei.ttc", 16)
-    # usr_font = ImageFont.truetype("/usr/share/fonts/truetype/takao/TakaoPGothic.ttf", 25)
-    d_usr = ImageDraw.Draw(image)
-    d_usr.text((0, 0), u"字", (0,0,0), font=usr_font)
-    get_pix(image)
-    get_gb2312_pix('\xd7\xd6', 16, 16, usr_font)
-    image.save("test.jpg")
+    # # usr_font = ImageFont.truetype("/usr/share/fonts/truetype/takao/TakaoPGothic.ttf", 25)
+    # d_usr = ImageDraw.Draw(image)
+    # d_usr.text((0, 0), u"字", (0,0,0), font=usr_font)
+    # get_pix(image)
+    with open('font16.tmp', 'wb') as outfile:
+        # get_gb2312_pix('\xd7\xd6', 16, 16, usr_font)
+        for i in range(0xA1, 0xF8):
+            for j in range(0xA1, 0xFF):
+                data = get_gb2312_pix(chr(i) + chr(j), 16, 16, usr_font)
+                data.tofile(outfile)
 
 if __name__ == '__main__':
     main()
