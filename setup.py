aba477cb0f25067958d24ae5f5af6d3771cb450a
mingang.he <dustgle@gmail.com>
2014-03-31 18:31:34 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="gendotmatrix",
+    version="1.0",
+    author="hmgle",
+    author_email="dustgle@gmail.com",
+    description=(
+        """Generate dot matrix font from vector font"""
+    ),
+    license="MIT",
+    url="https://github.com/hmgle/gendotmatrix",
+    install_requires = ['bitarray', 'PIL', 'getopt'],
+    scripts=['gendotmatrix.py']
+)
